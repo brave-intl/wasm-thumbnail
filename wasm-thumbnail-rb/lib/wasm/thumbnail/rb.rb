074b4734a098691bb1d2dbfc4701e34e9ab31b14
Tyler Smart <tsmart@brave.com>
2021-07-26 10:31:39 -0700

    Adding ruby gem

---
+++ wasm-thumbnail-rb/lib/wasm/thumbnail/rb.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rb/version"
+
+module Wasm
+  module Thumbnail
+    module Rb
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ wasm-thumbnail-rb/test/wasm/thumbnail/rb_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Wasm::Thumbnail::RbTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Wasm::Thumbnail::Rb::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
