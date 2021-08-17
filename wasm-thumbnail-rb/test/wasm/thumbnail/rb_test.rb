40dd37b882beea711036c152b76adf4b1b37cf90
Tyler Smart <tsmart@brave.com>
2021-08-17 09:50:56 -0700

    Run resize in a thread so ruby will GC it
    Ruby does not GC the wasm instance in creators unless we run this in a
    thread and extract the value.
    
    Also added a test for resizing a photo multiple times in order to test
    crashing, memory use, etc.

--- wasm-thumbnail-rb/test/wasm/thumbnail/rb_test.rb
+++ wasm-thumbnail-rb/test/wasm/thumbnail/rb_test.rb
@@ -36,4 +36,26 @@ class Wasm::Thumbnail::RbTest < Minitest::Test
     end
     assert_equal(exception.message, "Error processing the image.")
   end
+
+  def test_should_not_crash_after_many_resizes
+    # Photo from https://unsplash.com/photos/SRFhLQ_zmak
+    file_bytes = File.binread("#{__dir__}/shipwreck.jpg").unpack("C*")
+    quality_to_try = 100
+    # Run a few times. Adjust as needed, checking memory usage as well
+    while quality_to_try > 96
+      begin
+        return Wasm::Thumbnail::Rb.resize_and_pad(
+          file_bytes: file_bytes,
+          width: 2700,
+          height: 528,
+          size: 5,
+          quality: quality_to_try
+        )
+      rescue RuntimeError => e
+        quality_to_try -= 1
+        puts "Trying quality #{quality_to_try}"
+        next
+      end
+    end
+  end
 end
